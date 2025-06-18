6d23bafec90f79e49a2a4371b3bf257888061f16
EmilianoChavarria <20223tn089@utez.edu.mx>
2025-06-18 12:17:59 -0600

    Added getAll projects service and ended diesign of card

--- src/services/ProjectService.jsx
+++ src/services/ProjectService.jsx
@@ -1,5 +1,16 @@
+import { URL } from "../environments/global";
+
 export const ProjectService = {
-    getAll: async () => {
-      const result = await fetch('')
+  getAll: async () => {
+    try {
+      const response = await fetch(`${URL}/getAll`);
+      if (!response.ok) {
+        throw new Error(`Error al obtener proyectos: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error en ProjectService.getAll:", error);
     }
-}
+  }
+};
--- src/views/Projects/ProjectsView.jsx
+++ src/views/Projects/ProjectsView.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CardProject } from '../../components/projects/CardProject'
+import { ProjectService } from '../../services/ProjectService'
 
-export const ProjectsView = () => {
-    return (
-        <>
-            <section>
-                <header className='flex justify-between items-center mb-6'>
-                    <span className='text-xl font-semibold'>Listado de proyectos</span>
-                    <button className='bg-blue-500 text-white py-2 px-4 rounded-lg'>Agregar proyecto</button>
-                </header>
-                <main className='grid grid-cols-3 gap-6'>
-                    <CardProject/>
-                    <CardProject/>
-                    <CardProject/>
-                    <CardProject/>
-                </main>
-            </section>
-        </>
-    )
+export const ProjectsView  = () => {
+  const [projects, setProjects] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchProjects = async () => {
+    const data = await ProjectService.getAll()
+    console.log(data.projects)
+    if (data) setProjects(data.projects)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchProjects()
+  }, [])
+
+  return (
+    <>
+      <section>
+        <header className='flex justify-between items-center mb-6'>
+          <span className='text-xl font-semibold'>Listado de proyectos</span>
+          <button className='bg-blue-500 text-white py-2 px-4 rounded-lg'>Agregar proyecto</button>
+        </header>
+
+        {isLoading ? (
+          <p className='text-center text-gray-600'>Cargando proyectos...</p>
+        ) : (
+          <main className='grid grid-cols-3 gap-6'>
+            {projects.map((project) => (
+              <CardProject key={project.id} project={project} />
+            ))}
+          </main>
+        )}
+      </section>
+    </>
+  )
 }
